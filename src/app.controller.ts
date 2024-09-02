e1a160d95ae0d49f8288827109eff16e2606fda0
Joaquin Metalabs <joaquin@metalabs.company>
2024-09-02 21:45:27 +0700

    connect mogoose

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('home')
+  handleHomePage() {
+    // return this.appService.getHello();
+    console.log(this.configService.get('PORT'));
+    const message = this.appService.getHello();
+    return { message };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserController } from './user.controller';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://thunv:lnA5VrMSLjSF4zWT@cluster0.48miq.mongodb.net/',
+    // ),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
